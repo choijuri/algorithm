25115f9d3d618805b905e924f6391b91e140d89e
choijuri <chlwnfl52@gmail.com>
2019-01-22 18:13:29 +0900

    Algorithm

---
+++ src/main/java/Algorithm10.java
@@ -0,0 +1,18 @@
+
+
+
+
+public class Algorithm10 {
+/*문자열정수로 바꾸기
+https://programmers.co.kr/learn/courses/30/lessons/12925?language=java
+
+ */
+
+    class Solution {
+        public int solution(String s) {
+            int answer = 0;
+            answer = Integer.parseInt(s);
+            return answer;
+        }
+    }
+}
