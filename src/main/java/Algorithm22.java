dae8a2b1f7156868222ad1f05b409edea60db2ce
choijuri <chlwnfl52@gmail.com>
2019-02-17 01:38:21 +0900

    update Algorithm22

---
+++ src/main/java/Algorithm22.java
@@ -0,0 +1,27 @@
+public class Algorithm22 {
+    /* 
+     */
+    class Solution {
+        boolean solution(String s) {
+            boolean answer = true;
+            int p=0, y=0;
+
+            for(int a=0; a<s.length(); a++) {
+                if(s.charAt(a) == 'p' || s.charAt(a) == 'P') {
+                    p++;
+                }
+                if(s.charAt(a) == 'y' || s.charAt(a) == 'Y') {
+                    y++;
+                }
+            }
+
+            if(p == y) {
+                return answer;
+            } else {
+                answer = false;
+                return answer;
+            }
+
+        }
+    }
+}
