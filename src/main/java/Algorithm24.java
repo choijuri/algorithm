fd7053047874e1650996cfb70d6997f7b657d3a0
choijuri <45706567+choijuri@users.noreply.github.com>
2019-02-19 11:07:08 +0900

    Update Algorithm24.java

--- src/main/java/Algorithm24.java
+++ src/main/java/Algorithm24.java
@@ -1,4 +1,9 @@
 public class Algorithm24 {
+     /* 19.02.19 약수의합
+    https://programmers.co.kr/learn/courses/30/lessons/12928
+    
+    자연수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+     */
     class Solution {
         public int solution(int n) {
             int answer = 0;
