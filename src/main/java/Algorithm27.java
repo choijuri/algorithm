5662ba52a9a2d373493583359e294619316f2663
choijuri <45706567+choijuri@users.noreply.github.com>
2019-02-22 10:29:42 +0900

    Update Algorithm27.java

--- src/main/java/Algorithm27.java
+++ src/main/java/Algorithm27.java
@@ -1,5 +1,5 @@
-class Solution {
-/* 19.02.22  문자열 내림차순으로 배치하기
+public class Algorithm27 {
+  /* 19.02.22  문자열 내림차순으로 배치하기
 https://programmers.co.kr/learn/courses/30/lessons/12917?language=java
 
 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, 
@@ -7,6 +7,7 @@ solution을 완성해주세요.
 s는 영문 대소문자로만 구성되어 있으며, 
 대문자는 소문자보다 작은 것으로 간주합니다.
 */
+  class Solution {
   public String solution(String s) {
      String answer = "";
         String[] str = new String[s.length()];
@@ -20,4 +21,5 @@ s는 영문 대소문자로만 구성되어 있으며,
         }
         return answer;
   }
+}
 }
